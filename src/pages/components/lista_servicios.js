c3cab77d265ddf94ef81c4b616a75e04fedf86c8
Diego Elgueta <diegoelgueta2001@gmail.com>
2025-12-09 19:41:32 -0300

    ultimos arreglos

--- src/pages/components/lista_servicios.js
+++ src/pages/components/lista_servicios.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import Axios from 'axios';
-import { Link } from 'react-router-dom';   // 👈 NUEVO
+import { Link } from 'react-router-dom';
 
 function ListaServicios() {
 
